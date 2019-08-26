55d58ad14aaccb4b7782d77e34a2488e7d257610
Your Name <you@example.com>
2019-08-26 13:58:48 +0530

    first page

---
+++ script.js
@@ -0,0 +1,23 @@
+$(".txtb").on("keyup" , function(e){
+  //     13 means enter botton
+      if(e.keyCode == 13 && $(".txtb").val() !=  "")
+        {
+         var task = $("<div class='task'></div>").text($(".txtb").val());
+         var del = $("<i class='fas fa-trash-alt'></i>").click(function(){
+           task.hide();
+          });
+          
+          var check = $("<i class='fas fa-check'></i>").click(function(){
+             $(".comp").append(task)
+          });
+          
+          var edit = $("<i class='fas fa-edit'></i>").click(function(){    
+          $(".txtb").val(task.text());
+          })
+          
+          task.append(del,check,edit);
+          
+          $(".notcomp").append(task);
+          $(".txtb").val("");
+        }
+  });
